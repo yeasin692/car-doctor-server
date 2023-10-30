0e79a46daa940bc01a9a5e249cd221a7fca62d84
yeasin692 <mdyeasin94987@gmail.com>
2023-10-30 22:45:45 +0600

    cookie setion

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
@@ -27,7 +28,23 @@ async function run() {
 
     const serviceCollection = client.db("carsDoctor").collection("services");
     const bookingCollection = client.db("carsDoctor").collection("bookings");
-
+   
+    //auth related api
+    app.post('/jwt', async(req, res) =>{
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+      
+      res
+      .cookie('token', token, {
+        httpOnly: true,
+        secure: false,
+        sameSite: 'none'
+      })
+      .send({success: true});
+    })
+   
+    //services related api
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
